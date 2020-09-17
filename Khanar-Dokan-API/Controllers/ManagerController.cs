707e703acf6be4323bb1c4b844bac9042ae451c3
AswadMuntasir <aswadmuntasir04@gmail.com>
2020-09-18 01:20:55 +0600

    FIrst push
    Only Files are created.

---
+++ Khanar-Dokan-API/Controllers/ManagerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Khanar_Dokan_API.Controllers
+{
+    [RoutePrefix("manager")]
+    public class ManagerController : ApiController
+    {
+
+    }
+}
---
+++ Khanar-Dokan-API/Models/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Khanar_Dokan_API.Models
+{
+    public class Order
+    {
+        [Key]
+        public int oId { get; set; }
+
+        public int hId { get; set; }
+
+        public int uId { get; set; }
+
+        public string oStatus { get; set; }
+
+
+        public virtual History History { get; set; }
+        public virtual User User { get; set; }
+    }
+}
